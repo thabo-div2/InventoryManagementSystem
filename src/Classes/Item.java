0549d182e49009e8d42135efcb6943652a7d16c4
Thabo Setsubi <thabosetsubi3@gmail.com>
2024-07-31 16:19:36 +0300

    Merge branch 'part_2' of https://github.com/VCCT-PROG5121-2024-G4/ST10445734_PROG5121_PART_ONE
    # Conflicts:
    #	build/test/classes/LoginTest.class

---
+++ src/Classes/Item.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+import Enums.Category;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Item
+{
+
+    String name;
+    double price;
+    Category category;
+
+    public Item(String name, double price, Category cat)
+    {
+        this.name = name;
+        this.price = price;
+        this.category = cat;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public Category getCategory()
+    {
+        return category;
+    }
+}
---
+++ src/Enums/Category.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Enums;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public enum Category
+{
+    FRUITS, 
+    VEGETABLES, 
+    DAIRY, 
+    BAKERY, 
+    MEAT
+}
